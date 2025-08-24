3d27884b4f325e7d1b642b007d048e1d7bd0f7c4
Rajeshwar <9793anjalraj@gmail.com>
2025-08-24 18:07:01 +0530

    Prompt generator added

--- Langchain-Prompts/prompts_ui.py
+++ Langchain-Prompts/prompts_ui.py
@@ -1,7 +1,7 @@
 import os
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 
 #setuping the download path
@@ -36,22 +36,8 @@ length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraph
                                                            "Long (detailed explanation)"] )
 
 
-template = PromptTemplate(
-    template="""
-    Please summarize the research paper titled {paper_input} with the following specifications:
-    Explanation Style: {style_input}  
-    Explanation Length: {length_input}  
-    1. Mathematical Details:     
-        - Include relevant mathematical equations if present in the paper.  
-        - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-    2. Analogies:  
-        - Use relatable analogies to simplify complex ideas.  
-    If certain information is not available in the paper, respond with: Insufficient information available instead of guessing.  
-    Ensure the summary is clear, accurate, and aligned with the provided style and length.
-    """,
-    input_variables=['paper_input', 'style_input', 'length_input']
-)
 
+template = load_prompt('template.json')
 
 #filling the place holder
 prompt = template.invoke(
